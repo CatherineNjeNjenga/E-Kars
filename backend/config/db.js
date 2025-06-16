38c1152f9f9491906b235256e39053e7b5cabcbc
Catherine Njenga <freshiah.catherine@gmail.com>
2025-06-16 23:56:06 +0300

    updating dependancies

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,9 +5,9 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useCreateIndex: true,
+      // useCreateIndex: true,
       useUnifiedTopology: true,
-      useFindAndModify: true,
+      // useFindAndModify: true,
     });
     console.log("MongoDB connection SUCCESS");
   } catch (error) {
