770ca7dcca8da70528d3a297184744e84dc17151
Catherine Njenga <freshiah.catherine@gmail.com>
2025-06-19 01:27:21 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,13 +18,14 @@ app.use("/api/stripe", require("./routes/paymentRoutes"));
 app.use(errorHandler);
 app.use(express.static(path.join(__dirname, "frontend/build")));
 
-// const __dirname = path.resolve()
+const resolvedDirectory = path.resolve()
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'frontend/build')))
+  // app.use(express.static(path.join(__dirname, 'frontend/build')))
+  app.use(express.static(path.join(resolvedDirectory, '/frontend/build')))
   // app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(resolvedDirectory, 'frontend', 'build', 'index.html'))
     // res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
   });
 }
