600dcf3a31fb7d1750a244e9abd4d73e5fb6facf
Catherine Njenga <freshiah.catherine@gmail.com>
2025-06-18 22:44:21 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,13 +16,12 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/private", require("./routes/privateRoutes"));
 app.use("/api/stripe", require("./routes/paymentRoutes"));
 app.use(errorHandler);
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
-    // res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
-    res.sendFile('index.html');
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
   });
 }
 
