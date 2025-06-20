c3c6da9ef351c9a210b5c9371125715f46dcfe56
Catherine Njenga <freshiah.catherine@gmail.com>
2025-06-20 15:31:06 +0300

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,15 +1,17 @@
 const nodemailer = require("nodemailer");
+const Transport = require('nodemailer-brevo-transport');
 
 const sendEmail = (options) => {
-  const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE,
-    port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_STATUS,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  const transporter = nodemailer.createTransport(new Transport({
+    apiKey: process.env.BREVO_API_KEY
+    // service: process.env.EMAIL_SERVICE,
+    // port: process.env.EMAIL_PORT,
+    // secure: process.env.EMAIL_STATUS,
+    // auth: {
+      // user: process.env.EMAIL_USERNAME,
+      // pass: process.env.EMAIL_PASSWORD,
+    // },
+  }));
 
   const mailOptions = {
     from: process.env.EMAIL_FROM,
