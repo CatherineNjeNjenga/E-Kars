418369a38784bfe100a03c03234d1f4dc612c75e
Catherine Njenga <freshiah.catherine@gmail.com>
2025-06-20 13:43:55 +0300

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,6 +3,8 @@ const nodemailer = require("nodemailer");
 const sendEmail = (options) => {
   const transporter = nodemailer.createTransport({
     service: process.env.EMAIL_SERVICE,
+    port: process.env.EMAIL_PORT,
+    secure: process.env.EMAIL_STATUS,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
